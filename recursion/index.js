f9b00c21bca37a95eaf7dce084cb182eeab83263
Bruno Scherer <bruno.raphael96@gmail.com>
2020-01-20 09:00:29 -0300

    implement recursive array sum

--- recursion/index.js
+++ recursion/index.js
@@ -8,3 +8,7 @@ countdown(21)
 const factorial = (number) => number === 1 ? 1 : number * factorial(number - 1)
 
 console.log(factorial(5))
+
+const sumArray = (array) => !array.length ? 0 : array.pop() + sumArray(array)
+
+console.log(sumArray([2,4,6,20]))
